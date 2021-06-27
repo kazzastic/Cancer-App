1bbd6e32a246ec03f823ea97d3b0ceb11d1d9b08
kazzastic <rkazim79@gmail.com>
2021-06-27 07:47:58 +0500

    I deployed with local server

--- src/environment/index.js
+++ src/environment/index.js
@@ -2,5 +2,5 @@ const axios = require("axios");
 let prod = "https://onco-api.herokuapp.com/";
 let local = "http://127.0.0.1:5000/";
 export const flaskBackendService = axios.create({
-  baseURL: local
+  baseURL: prod
 });
